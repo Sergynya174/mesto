2c44edf965d42bd880ea82fac8215c2218caa650
Sergynya174 <Sergynya174@yandex.ru>
2021-12-28 19:28:50 +0300

    4 проеткная работа

---
+++ scripts/scripts.js
@@ -0,0 +1,31 @@
+const btnClose = document.querySelector('.popup__close-btn');
+const btnSave = document.querySelector('.popup__save-btn');
+const btnEdit = document.querySelector('.profile__edit-button');
+const btnAdd = document.querySelector('.profile__add-button');
+const btnLike = document.querySelector('.elements__like');
+let popup = document.querySelector('.popup');
+const popupOpened = 'popup_opened';
+let inputName = document.querySelector('#name');
+let inputJob = document.querySelector('#job');
+let nameUser = document.querySelector('.profile__title');
+let jobUser = document.querySelector('.profile__subtitle');
+let formElement = document.querySelector('.popup__container')
+
+
+btnEdit.addEventListener('click', function(){
+    popup.classList.add(popupOpened);
+});
+
+btnClose.addEventListener('click', function(){
+    popup.classList.remove(popupOpened);
+});
+
+function formSubmitHandler (evt){
+    evt.preventDefault();
+    nameUser.textContent = inputName.value;
+    jobUser.textContent = inputJob.value;
+    popup.classList.remove(popupOpened);
+};
+
+formElement.addEventListener('submit', formSubmitHandler);
+
